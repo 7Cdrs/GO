ac17509dad51bc56efe1a12a8c2b86160cde5299
HazelMpR <hazelmpr368@gmail.com>
2025-07-13 23:30:24 +0700

    variable

---
+++ string.go
@@ -0,0 +1,10 @@
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println(len ("Hazel"))
+	fmt.Println("Hazel MpR"[0])
+	fmt.Println("Hazel Mangadaralam Pratama Rayes"[1])
+
+}
---
+++ variable.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string 
+
+	name = "Hazel"
+	fmt.Println(name)
+
+	name = "Hazel MpR"
+	fmt.Println(name)
+
+	var friendName  = "Hazel Mangadaralam Pratama Rayes"
+	fmt.Println(friendName) 
+
+	var age int = 25
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+
+	var (
+		firstName = "Hazel"
+		lastName  = "Rayes"
+		
+
+	)
+	fmt.Println(firstName, lastName)
+	
+
+
+
+}
