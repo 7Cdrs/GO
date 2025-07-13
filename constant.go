bec34d1fbbc44fd8202e05c66842891015c89ac9
HazelMpR <hazelmpr368@gmail.com>
2025-07-14 00:15:44 +0700

    constant

---
+++ constant.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const( 
+		
+		firstName string = "Hazel"  
+	 	lastName = "Rayes"
+	 	value = 1000
+	)
+	fmt.Println(firstName, lastName)
+	fmt.Println(value)
+}
