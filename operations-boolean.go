01bdb3dce904fa844743561eca7c32f4f7a2c8de
HazelMpR <hazelmpr368@gmail.com>
2025-07-15 23:13:14 +0700

    boolean  operations

---
+++ operations-boolean.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt"
+
+func main() {
+
+	var Ujian  = 80
+	var Absensi = 70
+
+	var LulusUjian = Ujian > 75
+	var LulusAbsensi = Absensi > 70
+
+	var Lulus = LulusUjian && LulusAbsensi
+	fmt.Println("Apakah siswa lulus ujian?", LulusUjian)
+	fmt.Println("Apakah siswa lulus absensi?", LulusAbsensi)
+	fmt.Println("Apakah siswa lulus semua syarat?", Lulus)
+
+}
